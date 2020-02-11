97fc07fdcc3ba0bf28432f7bb7aa79d25824685a
ludorum <soosmail@me.com>
2020-02-11 21:32:25 +0900

    update script.

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -1,5 +1,5 @@
 # gitbook 의존 파일을 설치하고 gitbook 빌드를 돌린다.
-gitbook install && gitbook build
+gitbook install pages && gitbook build pages
 
 # github pages가 바라보는 gh-pages 브랜치를 만든다.
 git checkout gh-pages
@@ -8,11 +8,12 @@ git checkout gh-pages
 git pull origin gh-pages --rebase
 
 # gitbook build로 생긴 _book폴더 아래 모든 정보를 현재 위치로 가져온다.
-cp -R _book/* .
+cp -R pages/_book/* .
 
 # node_modules폴더와 _book폴더를 지워준다.
 git clean -fx node_modules
-git clean -fx _book
+git clean -fx pages
+git clean -fx *.sh
 
 # NOQA
 git add .
