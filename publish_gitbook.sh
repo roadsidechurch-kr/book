3717e3d3338920277f52b1ff1ada9e943e32576a
ludorum <soosmail@me.com>
2020-02-12 14:06:48 +0900

    update publish command.

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -5,11 +5,11 @@ bookDIR="_book"
 [ -d $buildDIR ] && rm -rf $buildDIR;
 git clone git@github.com:roadsidechurch-kr/book.git --branch gitbook $buildDIR
 rsync -ah ./pages/ ./$buildDIR/ --delete --exclude .git;
+
+cd ./$buildDIR
 git add .
 git commit  -m "update"
 git push;
-
-cd ./$buildDIR
 
 git clone git@github.com:roadsidechurch-kr/book.git --branch gh-pages $bookDIR
 gitbook install && gitbook build
